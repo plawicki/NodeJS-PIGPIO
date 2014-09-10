4c6a326c6ac67fdc9c73bab94fba4e77e81c64ee
Patryk <patlaw96@gmail.com>
2014-09-10 18:58:45 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
+
 $(function() {
 
-	var socket = io.connect('http://' + location.host);
+    var socket = io.connect('http://' + location.host);
+    var gpioNumb = 17;
+    
+    socket.on("gpio", function(gpio){
+    	$('#gpioState').val(gpio.state);
+    });
 
     $( "#slider" ).slider({
       range: "min",
@@ -8,18 +14,27 @@ $(function() {
       min: 0,
       max: 255,
       slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value );
+        $( "#amount" ).val( "$" + ui.value );
         socket.emit('change power', ui.value);
       }
     });
 
+    $('#gpioChange').click(function(){
+        socket.emit('change gpio', $('#nr').val());
+    });
+    
+    $('#gpioRead').click(function(){
+    	socket.emit('read gpio', $('#nr').val());
+    });
+
     $('#on').click(function(){
-    	socket.emit('change state', 1);
+        socket.emit('change state', 1);
     });
 
     $('#off').click(function(){
-    	socket.emit('change state', 0);
+        socket.emit('change state', 0);
     });
 
     $( "#amount" ).val( $( "#slider-range-min" ).slider( "value" ) );
+
 });
